8b9882b1e626161bb06f0a09a3669bba69a519bd
Yunpei24 <nikiemajoshua24@gmail.com>
2025-08-30 11:43:43 +0000

    updating of cifar training script

--- scripts/run_cifar_training.py
+++ scripts/run_cifar_training.py
@@ -59,7 +59,7 @@ def main():
     )
     print(f"Dataset loaded with {num_classes} classes.")
 
-    # --- Model, Criterion, Optimizer, Scheduler ---
+    # Model, Criterion, Optimizer, Scheduler
     model = densenet_cifar(growth_rate=args.growth_rate, num_classes=num_classes).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(
